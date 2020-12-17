0410ba001b730ac711f99513caa414f87f7ade44
Niranjan Raje <raje.niranjan18@siesgst.ac.in>
2020-12-18 00:01:49 +0530

    Second Commit
    Frontend created

--- public/js/geolocate.js
+++ public/js/geolocate.js
@@ -1,11 +1,53 @@
+
+var state,city,temperature,summury,humidity,windspeed,isday;
+var data;
+
+// loading 
+
+window.addEventListener('load',()=>{
+    document.querySelector('.loading').style.display='none';
+})
+
+Dom_insight=document.getElementById('insight')
+Dom_location=document.getElementById('loc');
+Dom_temp=document.getElementById('temp')
+Dom_humid=document.getElementById('humid-data')       
+Dom_wind=document.getElementById('wind');
+Dom_typohum=document.getElementById('typo-hum')
+Dom_typowind=document.getElementById('typo-wind');
+
+
+var dom = [Dom_insight,Dom_location,Dom_temp,Dom_wind,Dom_humid,Dom_typohum,Dom_typowind];
+
+// Change text-color
+function change(user_color,dom){
+    for(var iterator=0;iterator<dom.length;iterator++){
+        dom[iterator].style.color=user_color;
+    }
+}
+
+// change background image 
+var hours=new Date().getHours();
+
+
+if(hours >= 19 || (hours>= 1 && hours<=4)){
+    document.body.style.backgroundImage="url('/images/night.jpg')";
+    change('rgb(165, 243, 243)',dom);
+}
+else if(hours<19 && hours>=16 ){
+    document.body.style.backgroundImage="url('/images/evening.jpg')"
+}
+else{
+    document.body.style.backgroundImage="url('/images/morning.jpg')"
+    change('black',dom);
+}
+
+
+
 if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(async (UserLocation)=>{
         latitude=UserLocation.coords.latitude
         longitude=UserLocation.coords.longitude
-
-        // Text-content of i.p fields
-        document.querySelector('.latitude').value=latitude
-        document.querySelector('.longitude').value=longitude
 
         // sending lat,lon data 
         data = {latitude,longitude}
@@ -24,7 +66,26 @@ if('geolocation' in navigator){
 
         const resp=await fetch(`/weather/${latitude},${longitude}`)
          .then(res => res.json())
-         .then(json => console.log(json))
+         .then(json => data=json);
+
+         // output data 
+
+         city=data.location.name;
+         state=data.location.region;
+         temperature=data.current.feelslike_c;
+         summury=data.current.condition.text;
+         humidity=data.current.humidity;
+         windspeed=data.current.wind_kph;
+         isday=data.current.is_day
          
+
+         // injecting into dom :
+         Dom_insight.textContent=summury;
+         Dom_location.textContent=city+","+state;
+         Dom_temp.textContent=temperature+String.fromCharCode(176)+'C';
+         Dom_humid.textContent=humidity+"%";
+         Dom_wind.textContent=windspeed+" KPH";
     })
 }
+
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,7 +8,8 @@ Router.get('/weather/:latitudelongitude',async (request,response)=>{
     const requestdata=request.params['latitudelongitude'].split(',');
     const lat=requestdata[0];
     const lon=requestdata[1];
-    const api_url=`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0ae99525b3ba4698d9620fee9d7a4d35`;
+    const api_url=`http://api.weatherapi.com/v1/current.json?key=4f1d202285bd4fa5830132842201712&q=${lat},${lon}`
+    //const api_url=`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0ae99525b3ba4698d9620fee9d7a4d35`;
     const api_data=await fetch(api_url);
     const json_api_data=await api_data.json();
     console.log(json_api_data);
